3756275a6cd7077c85507f214d831342d156701a
Jenan04 <jenanyasser1@gmail.com>
2025-07-29 13:33:03 +0300

    added a continue btn

--- js/components/buttons.js
+++ js/components/buttons.js
@@ -1,11 +1,14 @@
 import { createElement } from "./header.js";
-export function createRestartButton(onClickHandler) {
+export const createRestartButton =(onClickHandler) =>{
   const btn = createElement("button", [
-    "bg-gray-300",
-    "rounded",
-    "px-4",
-    "py-2",
-    "ml-2",
+    // "bg-gray-300",
+    // "rounded",
+    // "px-4",
+    // "py-2",
+    // "ml-2",
+    "px-6", "py-2", "bg-gray-200", "text-gray-800", "text-lg",
+  "border-2", "rounded-2xl", "font-semibold", "shadow",
+  "hover:bg-gray-300", "transition-colors", "duration-300"
   ]);
   btn.textContent = "Restart";
   btn.addEventListener("click", onClickHandler);
--- js/components/render.js
+++ js/components/render.js
@@ -1,18 +1,35 @@
 
 import { createCharacterCard } from "../components/cards.js";
-import { createResultSection } from "../components/result.js"; // إذا عندك دالة لنتيجة اللعبة
+import { createResultSection } from "../components/result.js"; 
+import { createConButton } from "../components/continueBtn.js";
 import { createRestartButton } from "../components/buttons.js";
 import { createElement ,elementsAppender} from "./header.js";
-export function renderGameUI(character, score, isCorrect, restartHandler) {
+export const renderGameUI =(character, score, isCorrect,conHandler, restartHandler) =>{
   const container = document.querySelector(".container");
 
   const card = createCharacterCard(character);
   const result = createResultSection(score, isCorrect);
+  const conBtn = createConButton(conHandler);
   const restartBtn = createRestartButton(restartHandler);
 
-  const controlsDiv = createElement("div", ["flex", "items-center", "mt-4"]);
-  controlsDiv.appendChild(restartBtn);
+  const controlsDiv = createElement("div", [
+    "flex", 
+    "items-center", 
+    "justify-center",
+    "gap-4",
+    "mt-4",]);
+   elementsAppender(controlsDiv, [conBtn, restartBtn])
+   const wrapper = createElement("div", [
+    "flex",
+    "flex-col",
+    "md:flex-row",
+    "items-center",
+    "justify-center",
+    "gap-8",
+    "mt-8",
+  ]);
+  elementsAppender(wrapper, [card, result]);
 
 
-  elementsAppender(container, [card, result, controlsDiv]);
+  elementsAppender(container, [wrapper, controlsDiv]);
 }
